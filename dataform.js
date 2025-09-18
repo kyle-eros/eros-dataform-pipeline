561f55f1efacc3bfb193d0ccbd47d3eaf59dc1cb
kyle-eros <kyle@erosops.com>
2025-09-17 23:28:21 -0600

    fix: set default project and fully qualify dataset creation for BigQuery compatibility

--- dataform.js
+++ dataform.js
@@ -1,3 +1,10 @@
 const { dataform } = require("@dataform/core");
 
+dataform.projectConfig = {
+  defaultProject: "of-scheduler-proj",
+  defaultLocation: "US",
+  defaultSchema: "eros_staging",
+  defaultAssertionDataset: "eros_assertions"
+};
+
 module.exports = dataform;
